f1e43d70c7680451aae06707f6c39d3cbd01f428
Ubuntu <ubuntu@ip-172-31-93-139.ec2.internal>
2019-07-01 20:13:01 +0000

    Connected to database and tested connection

--- app.py
+++ app.py
@@ -1,12 +1,23 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 # Instance of Flask
 app = Flask(__name__)
 
-@app.route()
-def hello():
-    return "Hello World"
+# Config Flask app and connect to database
+app.config["MONGO_DBNAME"] = 'onlineCookbook'
+app.config["MONGO_URI"] = os.getenv('MONGO_URI')
+
+# Instanse of PyMongo
+mongo = PyMongo(app)
+
+@app.route('/')
+
+@app.route('/get_recipes')
+def get_recipes():
+    return render_template("recipes.html", recipes=mongo.db.recipes.find())
 
 if __name__ == '__main__':
     app.run(host=os.getenv("IP", "0.0.0.0"),
