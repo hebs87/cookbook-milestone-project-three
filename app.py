9ea0e6d26274be5366e7b228d2c5dffb377ee01e
Ubuntu <ubuntu@ip-172-31-93-139.ec2.internal>
2019-07-04 17:33:13 +0000

    Added user register functionality in app.py

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-import os
-from flask import Flask, render_template, redirect, request, url_for
+import os, json, math
+import re
+from flask import Flask, render_template, redirect, request, session, g, url_for, flash, Markup
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from werkzeug.security import check_password_hash, generate_password_hash
 
 # Instance of Flask
 app = Flask(__name__)
@@ -9,6 +11,8 @@ app = Flask(__name__)
 # Config Flask app and connect to database
 app.config["MONGO_DBNAME"] = 'onlineCookbook'
 app.config["MONGO_URI"] = os.getenv('MONGO_URI')
+# Secret key to enable user sessions for login and flash messages - generates random string
+app.secret_key = os.urandom(24)
 
 # Instance of PyMongo
 mongo = PyMongo(app)
@@ -21,10 +25,61 @@ serves_coll = mongo.db.serves
 time_coll = mongo.db.time
 users_coll = mongo.db.userLogin
 
+# User login sessions/logout
 @app.route('/')
-def base():
-    return render_template("base.html")
+def index():
+    return render_template("index.html")
 
+@app.route('/login')
+def login():
+    return ''
+
+@app.route('/register', methods=["GET", "POST"])
+def register():
+    '''
+    Create new user/register new account
+    Checks that the user doesn't already exist, and that length of username
+    and password is between 6-15 characters
+    Uses generate_password_hash to hash user's password in the database
+    '''
+    if request.method == "POST":
+        
+        new_username = request.form.get('new_username').lower()
+        new_password = request.form.get("new_password")
+        
+        # Check username and password are alphanumeric
+        for letter in new_username:
+            if not letter.isalnum():
+                message = "Usernames must be alphanumeric only. Please try another one."
+                return message
+        for letter in new_password:
+            if not letter.isalnum():
+                message = "Passwords must be alphanumeric only. Please try another one."
+                return message
+        
+        # Check username and password are between 6-15 characters
+        if len(new_username) < 5 or len(new_username) > 15:
+            message = "Usernames must be between 5 and 15 characters. Please try again."
+            return message
+        if len(new_password) < 5 or len(new_password) > 15:
+            message = "Passwords must be between 5 and 15 characters. Please try again."
+            return message
+        
+        # Check if username already exists
+        existing_user = users_coll.find_one({"username": new_username})
+        if existing_user:
+            message = "Sorry, " + new_username + " is already taken! Please choose another one!"
+            return message
+        
+        # If all checks pass, add user to the database
+        users_coll.insert_one({
+            "username": new_username,
+            "password": generate_password_hash(new_password)
+        })
+        session['user'] = new_username
+        message = "Welcome " + new_username + ", you will now be logged in"
+        return message
+    
 @app.route('/get_recipes')
 def get_recipes():
     return render_template("recipes.html", recipes=recipes_coll.find())
