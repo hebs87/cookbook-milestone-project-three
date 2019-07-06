2532549dc29952d74856ef82779449591ab541ad
Ubuntu <ubuntu@ip-172-31-93-139.ec2.internal>
2019-07-06 08:30:07 +0000

    Imported sample images to img dir and added index.html image

--- static/js/script.js
+++ static/js/script.js
@@ -1,6 +1,7 @@
-//Initialize Mobile Side Nav
-$('.sidenav').sidenav();
-//Initialize Modal
-$('.modal').modal();
-//Initialize Tabs
-$('.tabs').tabs();
+// //Initialize Mobile Side Nav
+// $('.sidenav').sidenav();
+// //Initialize Modal
+// $('.modal').modal();
+// //Initialize Tabs
+// $('.tabs').tabs();
+M.AutoInit();
