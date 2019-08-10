ed5ee2786f732acb9b9102f01b97f5eb9c915460
Ubuntu <ubuntu@ip-172-31-93-139.ec2.internal>
2019-08-10 17:27:46 +0000

    Moved errors into separate folders and updated app routes and css

--- app.py
+++ app.py
@@ -844,17 +844,17 @@ ERROR HANDLERS
  # 404 error handler
 @app.errorhandler(404)
 def not_found_error(error):
-    return render_template('error_404.html'), 404
+    return render_template("errors/error_404.html"), 404
 
 # 505 error handler
 @app.errorhandler(500)
 def something_wrong_error(error):
-    return render_template('error_500.html'), 500
+    return render_template("errors/error_500.html"), 500
 
 # Catch all error handler
 @app.route("/<path:path>")
 def global_error(path):
-    return render_template("error_404.html"), 404
+    return render_template("errors/error_404.html"), 404
 
 if __name__ == '__main__':
     app.run(host=os.getenv("IP", "0.0.0.0"),
