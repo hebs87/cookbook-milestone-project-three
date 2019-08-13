eb831ca9ebc88f070dc7136041efc49df7213938
Ubuntu <ubuntu@ip-172-31-93-139.ec2.internal>
2019-08-13 19:38:08 +0000

    Added new Procfile for run.py file

--- run.py
+++ run.py
@@ -7,5 +7,4 @@ app = create_app(Config)
 # Run the app
 if __name__ == '__main__':
     app.run(host=os.getenv("IP", "0.0.0.0"),
-            port=int(os.getenv("PORT", "5000")),
-            debug=True)
+            port=int(os.getenv("PORT", "5000")))
