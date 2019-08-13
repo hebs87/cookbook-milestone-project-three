97779d403ebf895226746eed02f022f1eca0ced6
Ubuntu <ubuntu@ip-172-31-93-139.ec2.internal>
2019-08-13 22:18:42 +0000

    Moved error files to separate folder and update app routes

--- cookbook/errors/routes.py
+++ cookbook/errors/routes.py
@@ -12,10 +12,10 @@ ERROR HANDLERS
 # 404 error handler
 @errors.app_errorhandler(404)
 def not_found_error(error):
-    return render_template("error_404.html"), 404
+    return render_template("errors/error_404.html"), 404
 
 
 # 505 error handler
 @errors.app_errorhandler(500)
 def something_wrong_error(error):
-    return render_template("error_500.html"), 500
+    return render_template("errors/error_500.html"), 500
